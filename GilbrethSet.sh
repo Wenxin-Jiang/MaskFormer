a0d9d31f840151ee359bbd086a1700694e2aff72
Wenxin Jiang <wenxin_jiang@outlook.com>
2023-04-06 18:07:59 -0400

    Create GilbrethSet.sh

---
+++ GilbrethSet.sh
@@ -0,0 +1,11 @@
+module load anaconda/2020.11-py38
+conda activate maskformer-torch
+module load cuda/11.7.0
+module load gcc/6.3.0
+# conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
+# python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
+
+# cd MaskFormer
+# pip install -r requirements.txt
+# pip install opencv-python
+# wget https://dl.fbaipublicfiles.com/maskformer/panoptic-coco/maskformer_panoptic_R50_bs64_554k/model_final_6f60dc.pkl
